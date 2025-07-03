b1b4402f7eebd0d2e39bced373a92fc2e9e9bc11
Heini <heinricha766@gmail.com>
2025-07-03 18:44:03 +0200

    first commit

--- SVG study/script.js
+++ SVG study/script.js
@@ -1,4 +1,24 @@
 import * as func from '../Code Library/functions.js';
+import * as ren from '../Code Library/renderSvg.js';
 
-const distance = func.getDistance({x: 30, y:1200}, {x: -80, y: 200});
-console.log(distance.euclideanDistance)
+const mainAspect = {x: 100, y: 100}
+
+const renderEl = () => {
+
+  const origin = ren.circle({ class: `element-origin`, fill: 'red', r: .75, });
+  const body = ren.rect({ class: 'body', width: '10', height: '10'})
+
+  const element = ren.group({ class: 'element', nodes: [body, origin] })
+
+  return element
+  
+}
+
+const myElement = renderEl();
+
+const mainDs = ren.svg({
+  class: 'main-display',
+  viewBox: `${-mainAspect.x/2} ${-mainAspect.y/2} ${mainAspect.x} ${mainAspect.y}`,
+  nodes: [myElement]
+}) // child nodes are relative to the center
+document.body.appendChild(mainDs.el)

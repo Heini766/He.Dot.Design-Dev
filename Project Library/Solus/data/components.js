98cdc1665adf9a84dc60795e051ffa2a41c2b0f4
Heini <heinricha766@gmail.com>
2025-07-05 09:19:36 +0200

    added character base rig

--- Project Library/Solus/data/components.js
+++ Project Library/Solus/data/components.js
@@ -1,7 +1,20 @@
 import * as ren from '../../../Code Library/renderSvg.js';
 import { mainAspect } from "./properties.js";
 
+const renCharacter = () => {
+
+  const characterSize = 15;
+  const base = ren.rect({class: 'character-base', width: `${characterSize}`, height: `${characterSize}`})
+  
+  const body = ren.group({id: 'character-body', nodes: [base.el]})
+  const character = ren.group({id: 'character', nodes: [body.el]})
+  
+  return character
+  
+}
+const character = renCharacter();
+
 const mainDs = ren.svg({ class: 'main-display', viewBox: `${-mainAspect.x/2} ${-mainAspect.y/2} ${mainAspect.x} ${mainAspect.y}`,
-nodes: []
+nodes: [character]
 })
 document.body.appendChild(mainDs.el);

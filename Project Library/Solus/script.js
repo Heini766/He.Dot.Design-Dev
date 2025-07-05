62409d44cb1b602399642088003f32c900e34d3f
Heini <heinricha766@gmail.com>
2025-07-05 10:22:30 +0200

    added check direction

--- Project Library/Solus/script.js
+++ Project Library/Solus/script.js
@@ -1,1 +1,15 @@
 import * as comps from './data/components.js';
+
+function checkDirection(event) {
+  const keyMap = { a: 'Left', d: 'Right' };
+  return keyMap[event.key];
+}
+
+window.addEventListener('keydown', (event) => {
+
+  const direction = checkDirection(event);
+  
+  if (direction) {
+    console.log(direction)
+  }
+})

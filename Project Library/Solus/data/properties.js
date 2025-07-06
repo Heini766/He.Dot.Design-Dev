44d32235c131d00586d576884e666e95cb04b27f
Heini <heinricha766@gmail.com>
2025-07-06 10:48:56 +0200

    added custom css props

--- Project Library/Solus/data/properties.js
+++ Project Library/Solus/data/properties.js
@@ -1,5 +1,10 @@
+import { createCustomProp } from "../../../Code Library/functions.js";
+
 export const mainAspect = {x: 400, y: 300};
-
 export const speed = 2;
-
 export const characterSize = 15;
+
+createCustomProp({
+  primeColor: 'white',
+  bgColor: '#1f1f1f',
+})

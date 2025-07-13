d5e475eb6a519f67d86191808c01bc068e01f864
Heini <heinricha766@gmail.com>
2025-07-13 11:39:48 +0200

    added new file

---
+++ Project Library/Solus/data/eventListeners.js
@@ -0,0 +1,77 @@
+import * as func from '../../../Code Library/functions.js'
+
+export function addControllerMoveHandler(node, display, mainAspect, targetNode) {
+
+  const onMouseDown = (event) => {
+
+    const cursorRelPos = func.getRelativePosition(event, display);
+    const targetNodeSize = targetNode.getBBox();
+    const [x, y, scale] = func.extNumbers(targetNode.getAttribute('transform'));
+    const offset = func.getPositionOffset(cursorRelPos, {x, y});
+
+    const onMouseMove = (event) => {
+
+      const {x: relX, y: relY} = func.getRelativePosition(event, display);
+      const newX = func.clamp(
+        relX - offset.x, 
+        -mainAspect.x/2 + targetNodeSize.width*.25, 
+        mainAspect.x/2 - targetNodeSize.width*.6
+      );
+      const newY = func.clamp(
+        relY - offset.y,
+        -mainAspect.y + targetNodeSize.height * 1.25,
+        -targetNodeSize.height/2
+      );
+      targetNode.setAttribute('transform', `translate(${newX} ${newY}) scale(${scale})`);
+
+    };
+
+    const onMouseUp = () => {
+      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mouseup', onMouseUp);
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+  };
+
+  node.addEventListener('mousedown', onMouseDown);
+
+}
+
+export function addAimHandler(display, targetNode, parent) {
+
+  const onMouseDown = () => {
+
+    const { getRelativePosition, calcLineAngle, clamp, getDistance, getRadPoints, extNumbers } = func;
+    const maxDistance = props.mainAspect.y * .1;
+
+    const [parentX, parentY] = extNumbers(parent.getAttribute('transform'))
+    const [targetX, targetY] = extNumbers(targetNode.getAttribute('transform'))
+
+    const onMouseMove = (event) => {
+
+      const curRelPos = getRelativePosition(event, display);
+      const { degNor } = calcLineAngle(curRelPos, {x: parentX, y: parentY})
+      const distance = clamp(getDistance(curRelPos, {x: parentX, y: parentY}).euclideanDistance, 0, maxDistance)
+      const { x, y } = getRadPoints(degNor/360, distance)
+
+      const transform = `translate(${-x + targetX} ${Math.max(-y + targetY, targetY)})`
+
+      targetNode.setAttribute('transform', transform)
+      
+    }
+
+    const onMouseUp = () => {
+      targetNode.setAttribute('transform', `translate(${targetX} ${targetY})`)
+      window.removeEventListener('mousemove', onMouseMove)
+    }
+
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+
+  }
+
+  targetNode.addEventListener('mousedown', onMouseDown)
+  
+}

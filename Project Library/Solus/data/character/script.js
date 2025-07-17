12d023f65d64d20ec94107fae307a4b43858f02a
Heini <heinricha766@gmail.com>
2025-07-17 07:13:36 +0200

    new template setup

--- Project Library/Solus/data/character/script.js
+++ Project Library/Solus/data/character/script.js
@@ -1,7 +1,3 @@
-import * as ren from '../../../../Code Library/renderSvg.js';
+import { mainDs } from './data/components.js'
 
-const mainAspect = {x: 200, y: 200}
-export const originMarker = ren.circle({class: 'origin-marker', r: 1})
-
-const mainDs = ren.svg({id: 'mainDisplay', class: 'main-display', viewBox: `${-mainAspect.x/2} ${-mainAspect.y/2} ${mainAspect.x} ${mainAspect.y}`, nodes: [originMarker]})
 document.body.appendChild(mainDs.el)

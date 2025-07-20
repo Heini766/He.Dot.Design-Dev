178f3f17ab5c6617257806bf401006b1e8a8e276
Heini <heinricha766@gmail.com>
2025-07-20 18:56:16 +0200

    update template

--- Template/data/display/components.js
+++ Template/data/display/components.js
@@ -2,7 +2,7 @@ import * as ren from '../../../../../Code Library/renderSvg.js';
 
 const dsAspect = {x: 100, y: 100}
 
-const originMarker = ren.circle({class: `origin-marker`, r: 2})
+const originMarker = ren.circle({class: `origin-marker`, r: 1})
 export const mainDs = ren.svg({
   id: 'mainDisplay',
   class: 'main-display',

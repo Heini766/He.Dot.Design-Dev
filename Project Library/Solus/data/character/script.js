81bc99c03d4b4fd1d15611d64c3c0e50431909e2
Heini <heinricha766@gmail.com>
2025-07-20 19:00:33 +0200

    deleted all projects

--- Project Library/Solus/data/character/script.js
+++
@@ -1,8 +0,0 @@
-import { moveOrigin } from '../../../../Code Library/functions.js'
-import { mainDs, renCharacter } from './data/components.js'
-
-mainDs.el.appendChild(renCharacter(20).el)
-
-document.body.appendChild(mainDs.el)
-
-moveOrigin(document.getElementById('characterSolus'), document.getElementById('solusBase'), {x: 0, y: 0})

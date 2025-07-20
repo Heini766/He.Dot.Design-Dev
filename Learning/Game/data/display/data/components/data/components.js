a570eace23f4f7a8d05737c337a38fbfbcf4b28f
Heini <heinricha766@gmail.com>
2025-07-20 13:17:48 +0200

    grid setup done

--- Learning/Game/data/display/data/components/data/components.js
+++ Learning/Game/data/display/data/components/data/components.js
@@ -6,11 +6,39 @@ const originMarker = ren.circle({class: 'origin-marker', r: 2})
 
 export const renDisplay = () => {
 
+  const renBg = () => {
+
+  const renGrid = () => {
+
+    const totalLinesX = 9;
+    const spacingX = mainAspect.y/totalLinesX
+    const gridLine = ren.path({class: 'grid-line'})
+
+    let gridLines = [];
+    for (let i = 0; i <= totalLinesX; i += 1) {
+      const newLineX = gridLine.el.cloneNode();
+      const newLineY = gridLine.el.cloneNode();
+      newLineX.setAttribute('d', `M${-mainAspect.x/2} ${-mainAspect.y/2 + spacingX * i} ${mainAspect.x/2} ${-mainAspect.y/2 + spacingX * i}`)
+      newLineY.setAttribute('d', `M${-mainAspect.x/2 + spacingX * i} ${-mainAspect.y/2} ${-mainAspect.x/2 + spacingX * i} ${mainAspect.y/2}`)
+      gridLines.push(newLineX)
+      gridLines.push(newLineY)
+    }
+
+    const grid = ren.group({id: 'grid', nodes: gridLines})
+    return grid
+    
+  }
+
+    const bg = ren.group({id: 'display-bg', nodes: [renGrid().el]})
+    return bg
+    
+  }
+
   const mainDs = ren.svg({
     id: 'mainDisplay',
     class: 'main-display',
     viewBox: `${-mainAspect.x/2} ${-mainAspect.y/2} ${mainAspect.x} ${mainAspect.y}`,
-    nodes: [originMarker]
+    nodes: [renBg(), originMarker]
   })
 
   return mainDs

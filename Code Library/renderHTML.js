0b17228ad6452b6ea4b4e2455f3b6d2767e857c3
Heini <heinricha766@gmail.com>
2025-07-29 21:58:58 +0200

    updates

---
+++ Code Library/renderHTML.js
@@ -0,0 +1,54 @@
+const divEl = document.createElement('div');
+const headerEl = document.createElement('header');
+const secEl = document.createElement('section');
+
+function genContent(config, node) {
+
+  config.propNames.forEach((el, index) => {
+
+    if (el === 'nodes') {
+      config.propValues[index].forEach((el) => {
+        node.appendChild(el)
+      })
+    } else if (el === 'content') {
+      node.innerHTML = `${config.propValues[index]}`
+    } else {
+      node.setAttribute(`${el}`, `${config.propValues[index]}`)
+    }
+    
+  })
+  
+}
+
+export function div(data) {
+
+  const newDiv = divEl.cloneNode();
+  const config = {propNames: Object.keys(data), propValues: Object.values(data)};
+
+  genContent(config, newDiv)
+
+  return newDiv
+  
+}
+
+export function section(data) {
+
+  const newSec = secEl.cloneNode();
+  const config = {propNames: Object.keys(data), propValues: Object.values(data)};
+
+  genContent(config, newSec)
+
+  return newSec
+  
+}
+
+export function header(data) {
+
+  const newHeader = headerEl.cloneNode();
+  const config = {propNames: Object.keys(data), propValues: Object.values(data)};
+
+  genContent(config, newHeader)
+
+  return  newHeader
+  
+}
---
+++ Project Library/website/script.js
@@ -0,0 +1,3 @@
+import { headerSection } from "./data/Header/components.js";
+
+document.body.appendChild(headerSection)

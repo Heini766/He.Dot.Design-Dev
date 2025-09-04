15527dbd40329270ae91a95c0425f5bc4d9abc28
Heini <heinricha766@gmail.com>
2025-09-04 17:53:55 +0200

    uvShader update

--- Code Library OOP/uvShader.js
+++ Code Library OOP/uvShader.js
@@ -1,25 +1,59 @@
-export class renUVs {
-  uvCords = []
-  uvNodes = []
-  constructor(data) {
-    for (let y = 0; y <= data.y; y+= 1) {
-      for (let x = 0; x <= data.x; x+=1) {
-        const newCord = {x: x/data.x, y: y/data.y};
-        this.uvCords.push(newCord)
+export class UVMapper {
+  #uvCoordinates = [];
+  #uvNodes = [];
+
+  constructor(gridSize, shapeCallback) {
+    if (!gridSize || typeof gridSize.x !== 'number' || typeof gridSize.y !== 'number') {
+      throw new Error('gridSize must be an object with x and y properties');
+    }
+    
+    if (typeof shapeCallback !== 'function') {
+      throw new Error('shapeCallback must be a function');
+    }
+
+    this.#generateUVCoordinates(gridSize);
+    this.#createUVNodes(shapeCallback);
+  }
+
+  #generateUVCoordinates(gridSize) {
+    for (let y = 0; y <= gridSize.y; y += 1) {
+      for (let x = 0; x <= gridSize.x; x += 1) {
+        this.#uvCoordinates.push({
+          x: x / gridSize.x,
+          y: y / gridSize.y
+        });
       }
     }
   }
 
-  getUVs(callBack) {
-    this.uvCords.forEach((cord, index) => {
-      const shape = callBack(cord, index);
-      if (shape) this.uvNodes.push(shape.node)
-    })
+  #createUVNodes(shapeCallback) {
+    this.#uvCoordinates.forEach((coordinate, index) => {
+      const shape = shapeCallback(coordinate, index);
+      if (shape && shape.node) {
+        this.#uvNodes.push(shape.node);
+      }
+    });
   }
-  
-  getNodes(callBack) {
-    this.uvNodes.forEach((el, index) => {
-      callBack(el, this.uvCords[index])
-    })
+
+  forEachNode(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback must be a function');
+    }
+
+    this.#uvNodes.forEach((node, index) => {
+      callback(node, this.#uvCoordinates[index]);
+    });
+  }
+
+  get nodes() {
+    return [...this.#uvNodes]; // Return a copy to prevent external modification
+  }
+
+  get coordinates() {
+    return [...this.#uvCoordinates]; // Return a copy to prevent external modification
+  }
+
+  get size() {
+    return this.#uvNodes.length;
   }
 }

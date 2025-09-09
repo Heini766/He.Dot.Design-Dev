10036201e3f0489e8fa8ba1d9afe30790a709820
Heini <heinricha766@gmail.com>
2025-09-09 20:20:33 +0200

    success

--- Code Library OOP/renderSvg.js
+++ Code Library OOP/renderSvg.js
@@ -1,40 +1,49 @@
+function configureElement(node, config) {
+
+  if (!config) return;
+
+  for (const [key, value] of Object.entries(config)) {
+    if (key === 'nodes' && Array.isArray(value)) {
+      value.forEach(nodeToAppend => nodeToAppend && node.appendChild(nodeToAppend.node));
+    } else if (key === 'content') {
+      node.innerHTML = value;
+    } else {
+      node.setAttribute(key, value);
+    }
+  }
+  
+}
+
 export class SVG {
-  
+
   node = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  
-  constructor(config = {}) {
-    Object.entries(config).forEach(([key, value]) => {
-      this.node.setAttribute(key, value);
+
+  constructor(config) {
+    configureElement(this.node, config);
+  }
+
+  addNodes(dataFunction) {
+    const nodes = dataFunction();
+    nodes.forEach(item => {
+      this.node.appendChild(item.node);
+      const id = item.node.getAttribute('id');
+      if (id) this[id] = item;
     });
   }
 
-  addNodes(nodes) {
-    const nodeArray = typeof nodes === 'function' ? nodes() : nodes;
-    
-    nodeArray.forEach(node => {
-      const id = node.node?.getAttribute('id');
-      if (id) this[id] = node;
-      this.node.appendChild(node.node);
-    });
-    
-    return this; // For method chaining
+  ren(tag, data) {
+    const newElement = new SVGElement(tag, data);
+
+    newElement.addNodes = this.addNodes.bind(newElement);
+    return newElement;
   }
 
-  ren(tag, attributes = {}) {
-    const element = document.createElementNS('http://www.w3.org/2000/svg', tag);
-    
-    Object.entries(attributes).forEach(([key, value]) => {
-      element.setAttribute(key, value);
-    });
-    
-    return {
-      node: element,
-      addNodes: (nodes) => {
-        const nodeArray = typeof nodes === 'function' ? nodes() : nodes;
-        nodeArray.forEach(node => element.appendChild(node.node));
-        return element;
-      }
-    };
-  }
+}
 
+// Helper class for created elements, used by SVG and ren()
+class SVGElement {
+  constructor(tag, config) {
+    this.node = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    configureElement(this.node, config);
+  }
 }

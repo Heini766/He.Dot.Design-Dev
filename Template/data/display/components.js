d60f986cc60082ce0dfbf9ffa74c0acebaaa9d9d
Heini <heinricha766@gmail.com>
2025-09-09 20:21:09 +0200

    clean up

--- Template/data/display/components.js
+++ Template/data/display/components.js
@@ -3,25 +3,3 @@ import { SVG } from "../../../Code Library OOP/renderSvg.js";
 const aspect = {x: 100, y: 100}
 
 export const canvas = new SVG({id: 'canvas', class: 'canvas', viewBox: `0 0 ${aspect.x} ${aspect.y}`});
-
-canvas.addNodes(() => {
-
-  const container  = canvas.ren('g', {id: 'container'})
-  container.addNodes(() => {
-
-    let shapes = [];
-
-    for (let i = 0; i < 4; i++) {
-      const newShape = canvas.ren('rect', {id: `shape${i + 1}`})
-      shapes.push(newShape)
-    }
-
-    return shapes
-    
-  })
-
-  return [container]
-  
-})
-
-console.log(canvas.container.shape4.node)

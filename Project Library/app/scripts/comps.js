269b52260ebe3ede4ed6714c370825abfd53cab7
Heini <heinricha766@gmail.com>
2025-09-23 23:04:33 +0200

    baseAdded

--- Project Library/app/scripts/comps.js
+++ Project Library/app/scripts/comps.js
@@ -1,16 +1,13 @@
-import { HTML } from '../../../Code Library OOP/renderHTML.js';
-import { SVG } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/renderSvg.js';
+import { HTML } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/App/Code%20Library%20OOP/renderHTML.js';
+import { SVG } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/App/Code%20Library%20OOP/renderSvg.js';
 
 const display = new HTML('div', {id: 'uiContainer', class: 'ui-container'});
-const div = display.ren('div', {id: 'divRendered'})
-
-display.addNodes(() => {
-  return [div]
-})
-
-console.log(div)
+const canvas = new SVG({viewBox: `0 0 100 100`, class: 'canvas'});
 
 document.getElementById('mainSection').appendChild(display.node);
 
+display.addNodes(() => {
 
-
+  return [canvas]
+  
+})

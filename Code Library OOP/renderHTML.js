d55cf21e0a9eeb5209fe195ef7073200fd9b8ee5
Heini <heinricha766@gmail.com>
2025-09-23 23:05:11 +0200

    HTML updated

--- Code Library OOP/renderHTML.js
+++ Code Library OOP/renderHTML.js
@@ -1,5 +1,20 @@
+function configureElement(node, config) {
+
+  if (!config) return;
+
+  for (const [key, value] of Object.entries(config)) {
+    if (key === 'nodes' && Array.isArray(value)) {
+      value.forEach(nodeToAppend => nodeToAppend && node.appendChild(nodeToAppend.node));
+    } else if (key === 'content') {
+      node.innerHTML = value;
+    } else {
+      node.setAttribute(key, value);
+    }
+  }
+  
+}
+
 export class HTML {
-  node = undefined;
 
   constructor(tag, data = {}) {
     this.node = document.createElement(tag);
@@ -21,21 +36,28 @@ export class HTML {
     return this; // For method chaining
   }
 
-  addNodes(nodes) {
-    const nodeArray = Array.isArray(nodes) ? nodes : [nodes];
-    
-    nodeArray.forEach(node => {
-      const element = node.node || node;
-      const id = element.getAttribute?.('id');
-      
-      if (id) this[id] = node;
-      this.node.appendChild(element);
+  addNodes(dataFunction) {
+    const nodes = dataFunction();
+    nodes.forEach(item => {
+      this.node.appendChild(item.node);
+      const id = item.node.getAttribute('id');
+      if (id) this[id] = item;
     });
-    
-    return this; // For method chaining
   }
 
-  static create(tag, attributes = {}) {
-    return new HTML(tag, attributes);
+  ren(tag, data) {
+    const newElement = new HTMLElement(tag, data);
+
+    newElement.addNodes = this.addNodes.bind(newElement);
+    return newElement
+  }
+  
+}
+
+// Helper class for created elements, used by HTML and ren()
+class HTMLElement {
+  constructor(tag, config) {
+    this.node = document.createElement(tag);
+    configureElement(this.node, config);
   }
 }

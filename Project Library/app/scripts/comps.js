b327cae135bd236a58e33171e6365187dd70243f
Heini <heinricha766@gmail.com>
2025-09-23 22:39:11 +0200

    updatesNeedToFixHTML

--- Project Library/app/scripts/comps.js
+++ Project Library/app/scripts/comps.js
@@ -1,1 +1,7 @@
-const display = document.getElementById('mainSection');
+import { HTML } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/renderHTML.js';
+import { SVG } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/renderSvg.js';
+
+const display = new HTML('div', {id: 'uiContainer', class: 'ui-container'});
+
+document.getElementById('mainSection').appendChild(display.node)
+

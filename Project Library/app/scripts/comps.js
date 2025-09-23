34aecc537ecd154d6551b83aa526caaba2cbc616
Heini <heinricha766@gmail.com>
2025-09-23 22:55:24 +0200

    functions updated

--- Project Library/app/scripts/comps.js
+++ Project Library/app/scripts/comps.js
@@ -1,7 +1,16 @@
-import { HTML } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/renderHTML.js';
+import { HTML } from '../../../Code Library OOP/renderHTML.js';
 import { SVG } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/renderSvg.js';
 
 const display = new HTML('div', {id: 'uiContainer', class: 'ui-container'});
+const div = display.ren('div', {id: 'divRendered'})
 
-document.getElementById('mainSection').appendChild(display.node)
+display.addNodes(() => {
+  return [div]
+})
+
+console.log(div)
+
+document.getElementById('mainSection').appendChild(display.node);
+
+
 

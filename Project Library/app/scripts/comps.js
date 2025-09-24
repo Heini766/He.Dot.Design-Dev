b800038dd630832a519d8b2cf81ef935342bc0e3
Heini <heinricha766@gmail.com>
2025-09-24 11:52:42 +0200

    updates

--- Project Library/app/scripts/comps.js
+++ Project Library/app/scripts/comps.js
@@ -1,13 +1,40 @@
-import { HTML } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/App/Code%20Library%20OOP/renderHTML.js';
+import { HTML } from '../../../Code Library OOP/renderHTML.js';
 import { SVG } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/App/Code%20Library%20OOP/renderSvg.js';
 
 const display = new HTML('div', {id: 'uiContainer', class: 'ui-container'});
 const canvas = new SVG({viewBox: `0 0 100 100`, class: 'canvas'});
 
+const tools = ['Path', 'Rectangle']
+
+display.createListener({event: 'click', func: () => {
+  console.log('Nice Stuff!!')
+}});
+
+display.removeListener(1)
+
 document.getElementById('mainSection').appendChild(display.node);
 
 display.addNodes(() => {
 
-  return [canvas]
+  const toolBar = display.ren('div', {id: 'toolBar', class: 'tool-bar'});
+  const btnContainer = display.ren('div', {id: 'btnContainer', class: 'btn-container'});
+  const toolBtns = [];
+  tools.forEach((tool, i) => {
+    toolBtns.push(display.ren('button', {class: 'tool-btn', id: `toolBtn${i}`, content: `${tool}`}))
+  })
+
+  btnContainer.addNodes(() => {return toolBtns})
+  toolBar.addNodes(() => {
+    const toolFooter = display.ren('footer', {id: 'toolFooter', class: 'tool-footer'});
+    toolFooter.addNodes(() => {
+      const runBtn = display.ren('button', {id: 'runBtn', class: 'run-btn', content: 'Run'});
+      return [runBtn]
+    })
+    
+    return [btnContainer, toolFooter]
+  })
+  
+
+  return [toolBar, canvas]
   
 })
---
+++ Project Library/app/scripts/customProps.js
@@ -0,0 +1,5 @@
+import { createCustomProp } from 'https://cdn.jsdelivr.net/gh/Heini766/He.Dot.Design-Dev@refs/heads/main/Code%20Library%20OOP/functions.js';
+
+createCustomProp({
+  brandColor: `#2DCCD3`,
+})

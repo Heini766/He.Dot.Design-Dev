1e4f894b8655ea53924e06fa38dcfea020a73bd1
Heini <heinricha766@gmail.com>
2025-09-26 22:20:54 +0200

    clean up

--- Template/data/display/components.js
+++ Template/data/display/components.js
@@ -1,8 +1,12 @@
-import { HTML } from "../../../Code Library OOP/renderHTML.js";
 import { SVG } from "../../../Code Library OOP/renderSvg.js";
 
 const aspect = {x: 100, y: 100}
 
-const docHTML = new HTML();
 export const canvas = new SVG({id: 'canvas', class: 'canvas', viewBox: `0 0 ${aspect.x} ${aspect.y}`});
+
+const container = canvas.ren('g', {id: 'container', transform: `translate(${aspect.x/2} ${aspect.y/2})`});
+const shape = canvas.ren('circle', {class: 'shape', id: `shape`, r: 5});
+
+canvas.addNodes(() => {return [container]})
+container.addNodes(() => {return [shape]})
 

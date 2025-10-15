0a38351ebb0e65b34d25f36f60d4caedf8b8343d
Heini <heinricha766@gmail.com>
2025-10-15 23:27:44 +0200

    clean up

--- Template/data/display/components.js
+++ Template/data/display/components.js
@@ -11,39 +11,7 @@ export const runBtn = html.ren('button', {id: 'runBtn', class: 'run-button', con
 
 runBtn.node.addEventListener('click', btnFunc)
 
-const pData = genPathData([
-  {
-    vertex: [-10, -10],
-    outTangent: [20, 0]
-  }, {
-    vertex: [10, 10],
-    inTangent: [-30, 0]
-  }, {
-    vertex: [25, -25]
-  }
-])
 
-pData.node1.set({vertex: [-50, -50], outTangent: [0, 0]})
-
-const myFirstPath = canvas.ren('path', {class: 'path', d: pData.d})
-
-const pos = getPointAlongPath(myFirstPath.node, .25);
-
-const container = canvas.ren('g', {id: 'container', transform: `translate(${aspect.x/2} ${aspect.y/2})`});
-const shape = canvas.ren('circle', {class: 'shape', id: `shape`, r: 5, transform: `translate(${pos.x} ${pos.y})`});
-
-canvas.addNodes( [container])
-container.addNodes([shape, myFirstPath])
-
-let toggle = false;
 function btnFunc() {
-
-  if (toggle) {
-    pData.node3.set({vertex: [50, 50]}, myFirstPath.node)
-    toggle = false
-  } else {
-    pData.node3.set({vertex: [-20, 50]}, myFirstPath.node)
-    toggle = true
-  }
   
 }

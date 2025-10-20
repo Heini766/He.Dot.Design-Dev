1c0daa32fbe7805351a2d11329dfa5793df9fb49
Heini <heinricha766@gmail.com>
2025-10-20 21:47:26 +0200

    pAlngPath V2

--- Code Library OOP/utilities.js
+++ Code Library OOP/utilities.js
@@ -59,8 +59,8 @@ export class UVMapper {
   }
 
   processCords(callBack) {
-    this.uvCords.forEach((coordinate, index) => {
-      callBack(coordinate, index);
+    this.uvCords.forEach((coordinate, i) => {
+      callBack(coordinate, i);
     });
   }
   
@@ -71,14 +71,91 @@ export class PatternAlongPath {
   #data = {
   };
 
-  constructor(path, config) {
+  constructor(path, config = {}) {
     
     this.#data.refPath = path instanceof Node ? path : undefined;
     if (typeof(path) === 'string') [path] = document.querySelectorAll(path); // Finds reference path
 
-    this.#data.container = config ? config.container : console.error('container node is invalid', this.#data.container)
-    this.#data.count = config ? config.count : 3
+    this.#data.container = config.container ? config.container : console.error('container node is invalid', this.#data.container)
+    this.#data.count = config.count ? config.count : 3
+    this.#data.tangent = config.tangent ? config.tangent : false
         
+  }
+
+  put(defNode, arrayConfig = []) {
+
+    defNode = defNode instanceof Node ? defNode : console.error('default node is', defNode);
+    if (!defNode) return
+
+    if (!this.#data.pat) this.#data.pat = new Map();
+
+    for (let i = 0; i < this.#data.count; i++) {
+
+      const data = {}
+
+      data.offset = i/Math.abs(1 - this.#data.count);
+
+      let newNode
+      if (arrayConfig[i] ) {
+        newNode = arrayConfig[i].node instanceof Node ? arrayConfig[i].node : undefined;
+        data.tangent = arrayConfig[i].tangent ? arrayConfig[i].tangent : this.#data.tangent;
+        data.angle = arrayConfig[i].angle ? arrayConfig[i].angle : undefined;
+      } else {
+        data.tangent = this.#data.tangent
+      }
+      
+      data.node = newNode ? newNode.cloneNode(true) : defNode.cloneNode(true);
+      data.pos = getPointAlongPath(this.#data.refPath, data.offset);
+
+      this.#data.pat.set(`shape${i + 1}`, data)
+
+    }
+
+    this.#data.pat.forEach((v) => {
+
+      let transform = `translate(${v.pos[0]} ${v.pos[1]})`;
+      
+      v.angle = v.angle ? v.angle : getPointAngle(this.#data.refPath, v.offset).deg
+      if (v.tangent) transform += `rotate(${v.angle})`
+
+      v.node.setAttribute('transform', transform);
+      this.#data.container.appendChild(v.node)
+      
+    })
+    
+  }
+
+  set(callBack) {
+
+    if (!this.#data.pat || typeof(callBack) !== 'function') return
+
+    this.#data.pat.forEach((v, key, map) => {
+      callBack(v, key, map)
+    })
+
+    this.update()
+  }
+
+  update() {
+
+    this.#data.pat.forEach((v) => {
+
+      const newPos = getPointAlongPath(this.#data.refPath, v.offset)
+      v.pos = newPos
+
+      let translate = `translate(${v.pos[0]} ${v.pos[1]})`;
+      let rotate = v.angle ? `rotate(${v.angle})` : undefined;
+      
+      if (v.tangent) {
+        v.angle = getPointAngle(this.#data.refPath, v.offset).deg
+        rotate = `rotate(${v.angle})`
+      }
+
+      const transform = rotate ? translate + rotate : translate;
+      v.node.setAttribute('transform', transform);
+      
+    })
+    
   }
   
 }
--- Template/data/components/script.js
+++ Template/data/components/script.js
@@ -7,11 +7,43 @@ import { getRelativePosition } from "../../../Code Library OOP/functions.js";
 const myPath = canvas.ren('path', {id: 'path', class: 'path'});
 const myPData = genPathData(myPath.node, [
   {vtx: [20, 25]},
-  {vtx: [80, 75]}
+  {vtx: [50, 50]}
 ])
 
-const pattern = new PatternAlongPath(myPath.node, {container: canvas.node});
+const pattern = new PatternAlongPath(myPath.node, {container: canvas.node, count: 20});
 
-console.log(pattern)
+pattern.put(canvas.ren('rect', {width: 2, height: 2, fill: 'white'}).node, [
+  {node: canvas.ren('rect', {fill: 'white', width: 2, height: 2}).node}, 
+])
 
-canvas.addNodes(myPath)
+pattern.set((v, key, map) => {
+  map.get('shape18').node.style.fill = 'purple'
+})
+
+//canvas.addNodes(myPath)
+
+createDrag(canvas.node, {
+  onDown: (e) => {
+
+    const pos = getRelativePosition(e, canvas.node);
+
+    myPData.nodes.get('node1').set({
+      vtx: [pos[0], pos[1]]
+    })
+
+    pattern.update()
+    
+  },
+  onMove: e => {
+    const pos = getRelativePosition(e, canvas.node);
+
+    myPData.nodes.get('node1').set({
+      vtx: [pos[0], pos[1]]
+    })
+
+    pattern.update()
+  },
+  onUp: () => {
+    console.log(pattern)
+  }
+}).active(true)
